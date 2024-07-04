05ef1fc450c327542e7bfb2a6f5b52b5d178b866
jayanta dey <jayantadey398@gmail.com>
2024-07-04 14:11:48 +0530

    change the error message text

--- src/Components/ErrorPop/index.jsx
+++ src/Components/ErrorPop/index.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import "./style.css";
 import cross from "../../Assets/Images/blueCross.png";
 
+import { useTranslation } from 'react-i18next';
+
+
 export default function ErrorPop({ errorActive, setErrorPop, errorMsg }) {
+  const [t, i18n] = useTranslation("global");
+
   return (
     <div className='loaderBox' style={{ display: errorActive ? "flex" : "none" }}>
       <div className='errorPop'>
         <img src={cross} className='blueCross' onClick={() => setErrorPop(false)} />
-        <h2>Something went wrong  <br /> please try again!</h2>
+        <h2>{t("errorMsg.l1")}  <br /> {t("errorMsg.l2")} </h2>
       </div>
     </div>
   )
